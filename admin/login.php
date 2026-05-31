75f5b5ad33cb2d9626905bb8d4bddd2171555b63
lewisjn-arch <lhinga75@gmail.com>
2026-05-31 10:35:21 +0300

    Admin access

--- admin/login.php
+++ admin/login.php
@@ -1,51 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Valeria Villas Admin Login</title>
+        <link rel="stylesheet" href="assets/css/blog.css">
+    </head>
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Valeria Villas Admin Login</title>
-
-<link rel="stylesheet" href="assets/css/blog.css">
-
-</head>
-
-<body>
-
-<div class="admin-login-wrapper">
-
-    <div class="admin-login-card">
-
-        <h2>Valeria Villas</h2>
-
-        <p>Content Management System</p>
-
-        <form>
-
-            <input
-                type="text"
-                placeholder="Username"
-            >
-
-            <input
-                type="password"
-                placeholder="Password"
-            >
-
-            <button type="submit">
-
-                Login
-
-            </button>
-
-        </form>
-
-    </div>
-
-</div>
-
-</body>
+    <body>
+        <div class="admin-login-wrapper">
+            <div class="admin-login-card">
+                <img src="../../img/logo.webp" class="admin-logo" alt="Valeria Villas Logo">
+                <h2>Valeria Villas</h2>
+                <p>Content Management System</p>
+                <form>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </body>
 
 </html>
